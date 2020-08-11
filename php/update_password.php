32efc556e2eb63ed1160fba4f2fc1b7dec257876
waleedbutt98 <waleedbut1998@gmail.com>
2020-08-11 20:56:52 +0500

    update password.php done

--- php/update_password.php
+++ php/update_password.php
@@ -17,34 +17,69 @@ if(mysqli_connect_error())
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$sql = " SELECT password FROM user_info WHERE email='$email' ";
+$email = mysqli_real_escape_string($conn,$email);
+$password = mysqli_real_escape_string($conn,$password);
+$opassword = mysqli_real_escape_string($conn,$opassword);
 
+$sql = " SELECT password FROM user_info WHERE email=? ";
 
-$result = mysqli_query($conn, $sql);
+// Preparing Template
+$selectresult = mysqli_prepare($conn,$sql);
 
-$row = mysqli_fetch_all($result);
+// Binding Variables
+mysqli_stmt_bind_param($selectresult,"s",$email);
 
-if(mysqli_num_rows($result) <= 0)
+// Binding Result
+$pass_rec = "";
+mysqli_stmt_bind_result($selectresult,$pass_rec);
+
+// Executing Statement
+mysqli_stmt_execute($selectresult);
+
+//Storing Result
+mysqli_stmt_store_result($selectresult);
+
+// Fetching Result
+mysqli_stmt_fetch($selectresult);
+
+if(mysqli_stmt_num_rows($selectresult) <= 0)
 {
     echo "Email Does Not Exists";
+    mysqli_stmt_close($selectresult);
     mysqli_close($conn);
     die();
 }
-if($row[0][0] != $opassword)
+if($pass_rec != $opassword)
 {
     echo "Wrong Old Password";
+    mysqli_stmt_close($selectresult);
     mysqli_close($conn);
     die();
 }
 
+$sql = "UPDATE user_info Set password =? WHERE email=? AND password=?";
 
-$query = "UPDATE user_info Set password = '$password' WHERE email='$email' AND password='$opassword'";
+// Preparing Template
+$selectresult = mysqli_prepare($conn,$sql);
 
-if(mysqli_query($conn,$query))
+// Binding Variables
+mysqli_stmt_bind_param($selectresult,"sss",$password,$email,$opassword);
+
+// Executing Statement
+mysqli_stmt_execute($selectresult);
+
+//Storing Result
+mysqli_stmt_store_result($selectresult);
+
+// Fetching Result
+mysqli_stmt_fetch($selectresult);
+
+if(mysqli_stmt_num_rows($selectresult) <= 0)
     echo "Updated";
 else
     echo "Failed To Update";
 
 mysqli_close($conn);
+mysqli_stmt_close($selectresult);
 
 ?>
