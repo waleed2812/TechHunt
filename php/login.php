1df5c8f6347b68148e7d7cc4ee90d19ee6774c89
waleedbutt98 <waleedbut1998@gmail.com>
2020-08-11 17:36:44 +0500

    Login.php done

--- php/login.php
+++ php/login.php
@@ -11,27 +11,43 @@ if(mysqli_connect_error())
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$sql = " SELECT password FROM user_info WHERE email='$email' ";
+$email = mysqli_real_escape_string($conn,$email) ;
+$password = mysqli_real_escape_string($conn,$password) ;
 
+$sql = " SELECT password FROM user_info WHERE email=?";
 
-$result = mysqli_query($conn, $sql);
+// Preparing Template
+$selectresult = mysqli_prepare($conn,$sql);
 
-$row = mysqli_fetch_all($result);
+// Binding Variables
+mysqli_stmt_bind_param($selectresult,"s",$email);
 
-if(mysqli_num_rows($result) <= 0)
+//Binding Result
+$result = "";
+mysqli_stmt_bind_result($selectresult, $result);
+
+// Executing Statement
+mysqli_stmt_execute($selectresult);
+
+//Storing Result
+mysqli_stmt_store_result($selectresult);
+
+if(mysqli_stmt_num_rows($selectresult) <= 0)
 {
     echo "Email Does Not Exists";
-    mysqli_close($conn);
+    mysqli_stmt_close($selectresult);
     die();
 }
-if($row[0][0] != $password)
+
+mysqli_stmt_fetch($selectresult);
+if($result != $password)
 {
     echo "Password Does Not Match";
-    mysqli_close($conn);
+    mysqli_stmt_close($selectresult);
     die();
 }
 echo "Successfully Logged in";
 
-mysqli_close($conn);
+mysqli_stmt_close($selectresult);
 $_SESSION['email'] = $email;
 ?>
